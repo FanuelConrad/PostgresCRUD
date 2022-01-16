dec8a4f27535115cb977ddeefb3fbf0313dc6624
FanuelConrad <59276995+FanuelConrad@users.noreply.github.com>
2022-01-16 19:34:38 +0300

    Add project files.

---
+++ DataAccess/IDataAccessProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PostgresCRUD.Models;
+
+namespace PostgresCRUD.DataAccess
+{
+    public interface IDataAccessProvider
+    {
+        void AddPatientRecord(Patient patient);
+        void UpdatePatientRecord(Patient patient);
+        void DeletePatientRecord(string id);
+        Patient GetPatientSingleRecord(string id);
+        List<Patient> GetPatientRecords();
+    }
+}
